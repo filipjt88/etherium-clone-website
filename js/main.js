77a78acad6e3aad72f8f509b509d210b24129b00
Filip Jotic <33574046+filipjt88@users.noreply.github.com>
2021-09-07 23:37:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.element', {
+    strings: ['Arabic', 'Chinese', 'Croatian', 'Dutch', 'Finnish', 'French','English', 'Irtalian', 'Japanese'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false,
+});
